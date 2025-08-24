f82e66bb45f3c4a45f64f3498d195ec4abb4c39d
braynerdev <dev.brayner@outlook.com>
2025-08-24 11:08:47 -0300

    finalizado

--- Mobile/script.js
+++ Mobile/script.js
@@ -17,7 +17,7 @@ $("#data_final").on("change", function () {
 
 function requisicao(dt_inicial, dt_final){
     const datas = limparData(dt_inicial,dt_final);
-    const url = `https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL/?start_date=${datas['dt_inicial']}&end_date=${datas['dt_final']}`
+    const url = `https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL,CHF-BRL,ARS-BRL,BNB-BRL,AUD-BRL,BOB-BRL,COP-BRL,DOGE-BRL,ETH-BRL,GBP-BRL/?start_date=${datas['dt_inicial']}&end_date=${datas['dt_final']}`
     fetch(url)
     .then(response => response.json())
     .then(data =>{
@@ -30,18 +30,18 @@ function requisicao(dt_inicial, dt_final){
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">${moeda.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">high: ${moeda.high}</h6>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">low: ${moeda.low}</h6>
-                        <h6 class="card-subtitle mb-2 text-body-secondary cardaovivo"><span class="aovivo"></span> bid: ${moeda.bid}</h6>
-                        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="aovivo"></span> ask: ${moeda.ask}</h6>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">create date: ${moeda.create_date}</h6>
+                        <div class="card-subtitle text-body-secondary">Maior Valor: ${moeda.high}</div>
+                        <div class="card-subtitle text-body-secondary">Menor Valor: ${moeda.low}</div>
+                        <div class="card-subtitle text-body-secondary"><span class="aovivo"></span> Oferta de Compra: ${moeda.bid}</div>
+                        <div class="card-subtitle text-body-secondary"><span class="aovivo"></span> Oferta de venda: ${moeda.ask}</div>
                     </div>
                 </div>    
             `;
         };
+        div.style.display = "flex";
         div.scrollIntoView({
             "behavior": "smooth",
-            "block": "center"
+            "block": "start"
         });
     });
 }
