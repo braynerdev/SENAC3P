f6e1ac28726ee2e4d96fc1becf872d72f49a8147
braynerdev <dev.brayner@outlook.com>
2025-08-25 18:55:06 -0300

    ajuste js

--- Mobile/script.js
+++ Mobile/script.js
@@ -18,6 +18,7 @@ $("#data_final").on("change", function () {
 function requisicao(dt_inicial, dt_final){
     const datas = limparData(dt_inicial,dt_final);
     const url = `https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL,CHF-BRL,ARS-BRL,BNB-BRL,AUD-BRL,BOB-BRL,COP-BRL,DOGE-BRL,ETH-BRL,GBP-BRL/?start_date=${datas['dt_inicial']}&end_date=${datas['dt_final']}`
+    // sei que o correto seria criar as variaveis das coins e por na url de requisição sendo que eu estava com preguiça.
     fetch(url)
     .then(response => response.json())
     .then(data =>{
@@ -30,10 +31,10 @@ function requisicao(dt_inicial, dt_final){
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">${moeda.name}</h5>
-                        <div class="card-subtitle text-body-secondary">Maior Valor: ${moeda.high}</div>
-                        <div class="card-subtitle text-body-secondary">Menor Valor: ${moeda.low}</div>
-                        <div class="card-subtitle text-body-secondary"><span class="aovivo"></span> Oferta de Compra: ${moeda.bid}</div>
-                        <div class="card-subtitle text-body-secondary"><span class="aovivo"></span> Oferta de venda: ${moeda.ask}</div>
+                        <div class="card-subtitle text-body-secondary">Maior Valor: R$ ${moeda.high}</div>
+                        <div class="card-subtitle text-body-secondary">Menor Valor: R$ ${moeda.low}</div>
+                        <div class="card-subtitle text-body-secondary"><span class="aovivo"></span> Oferta de Compra: R$ ${moeda.bid}</div>
+                        <div class="card-subtitle text-body-secondary"><span class="aovivo"></span> Oferta de venda: R$ ${moeda.ask}</div>
                     </div>
                 </div>    
             `;
